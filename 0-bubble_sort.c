1252ac7cfebb3e95febe80531128c5bd1773b982
Ace <AceQuantumBit@gmail.com>
2023-12-20 17:59:46 -0600

    ?????? I know there's more to do but idk where or how

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,8 +16,9 @@ void bubble_sort(int *array, size_t size)
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                for (k = 0; k < size; k++)
+                for (k = 0; k < size - 1; k++)
                     printf("%d, ", array[k]);
+                printf("%d ", array[k]);
                 printf("\n");
             }
         j++;
