229e9dfe969df6ddc85f7887b4534dfcccc524c4
Ace <AceQuantumBit@gmail.com>
2023-12-20 17:50:08 -0600

    added comma in print and newline to notation file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
                 for (k = 0; k < size; k++)
-                    printf("%d ", array[k]);
+                    printf("%d, ", array[k]);
                 printf("\n");
             }
         j++;
