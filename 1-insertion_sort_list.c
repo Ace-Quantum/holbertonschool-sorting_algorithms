1d97e5b2a97488f440e7fca6a85552278700749d
Ace <AceQuantumBit@gmail.com>
2023-12-21 14:17:35 -0600

    the first one can always be considered sorted dummy

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tracknode = *list;
+    listint_t *tracknode = list->next;
 
     printf("tracknode val: %d\ntracknext val: %d\n", tracknode->n, tracknode->next->n);
     while (tracknode != NULL && tracknode->n < tracknode->next->n)
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
         printf("iterating track node\ntracknode value: %d, tracknext value: %d\n", tracknode->n, tracknode->next->n);
         tracknode = tracknode->next;
     }
-    while (tracknode != NULL && tracknode->n > tracknode->prev->n)
+    while (tracknode != NULL && tracknode->prev != NULL && tracknode->n > tracknode->prev->n)
     {
         swap_node_back_one(tracknode);
         print_list(*list);
