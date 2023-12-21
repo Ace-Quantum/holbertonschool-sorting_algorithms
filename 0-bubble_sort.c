c785bf05583402eaa44ecff1e3429d0633b6d680
Ace <AceQuantumBit@gmail.com>
2023-12-20 18:01:57 -0600

    Maybe this is it. Idk

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,10 +16,7 @@ void bubble_sort(int *array, size_t size)
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                for (k = 0; k < size - 1; k++)
-                    printf("%d, ", array[k]);
-                printf("%d ", array[k]);
-                printf("\n");
+                print_array(array, size);
             }
         j++;
         }
