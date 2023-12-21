3461a410790e081c02917be20f9399a68364d322
Ace <AceQuantumBit@gmail.com>
2023-12-21 13:46:21 -0600

    reset tracknode

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -18,7 +18,7 @@ void insertion_sort_list(listint_t **list)
             swap_node_back_one(tracknode);
             print_list(*list);
         }
-        *tracknode = *list;
+        tracknode = *list;
     }
     return;
 }
