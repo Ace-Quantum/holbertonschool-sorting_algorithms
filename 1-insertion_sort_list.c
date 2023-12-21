23f52b6c8f31483edfa81107a0984a8869c8ab6f
Ace <AceQuantumBit@gmail.com>
2023-12-21 11:46:14 -0600

    moved swap node

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -33,3 +33,16 @@ int getlist_len(const listint_t *h)
 	}
 	return (num_nodes);
 }
+
+void swap_node_back_one(listint_t *swap_node)
+{
+    listint_t temp_node;
+
+    swap_node->prev->next = swap_node->next;
+    swap_node->next->prev = swap_node->prev;
+
+    swap_node->prev = swap_node->prev->prev;
+    swap_node->prev->prev->next = swap_node;
+
+    return;
+}
