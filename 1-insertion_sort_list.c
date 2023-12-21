bdb5a70d1dec77f310ca6cd37161f3eb6062ceab
Ace <AceQuantumBit@gmail.com>
2023-12-21 11:46:57 -0600

    unused node

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -36,8 +36,6 @@ int getlist_len(const listint_t *h)
 
 void swap_node_back_one(listint_t *swap_node)
 {
-    listint_t temp_node;
-
     swap_node->prev->next = swap_node->next;
     swap_node->next->prev = swap_node->prev;
 
